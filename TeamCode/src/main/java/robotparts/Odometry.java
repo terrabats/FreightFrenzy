775f8a37419516c7b70a4f007f15ab6f343b088e
Akhil Agarwal <akhilagarwal79@gmail.com>
2021-11-20 17:33:47 -0800

    v1.6.14 - Corrected Odometry for d1 = d2

--- TeamCode/src/main/java/robotparts/Odometry.java
+++ TeamCode/src/main/java/robotparts/Odometry.java
@@ -34,10 +34,14 @@ public class Odometry extends RobotPart {
         double d1 = getDeltaOdoOne();
         double d2 = getDeltaOdoTwo();
         double theta = (d2 - d1)/R;
-        double r2 = d1/theta;
-        double r3 = sqrt(pow(ODO1_TO_CENTER_X - r2, 2) + pow(ODO1_TO_CENTER_Y, 2));
-        double dx = r3 * (1 - cos(theta));
-        double dy = r3 * sin(theta);
-        return new double[] { dx, dy, theta };
+        if (theta != 0) {
+            double r2 = d1 / theta;
+            double r3 = sqrt(pow(ODO1_TO_CENTER_X - r2, 2) + pow(ODO1_TO_CENTER_Y, 2));
+            double dx = r3 * (1 - cos(theta));
+            double dy = r3 * sin(theta);
+            return new double[]{dx, dy, theta};
+        } else {
+            return new double[] { 0, d1, 0 };
+        }
     }
 }
