6a50a45eb66b406508adc356b0c0dbc804f3be7f
Akhil Agarwal <akhilagarwal79@gmail.com>
2021-12-09 22:25:47 -0800

    v2.8.0 - Created decent Autonomous based on time - About 13 V is correct

--- TeamCode/src/main/java/auton/AutoTest.java
+++ TeamCode/src/main/java/auton/AutoTest.java
@@ -1,9 +1,44 @@
 package auton;
 
+import util.ExceptionCatcher;
+import util.Timer;
+import static global.General.*;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="AutoTest")
 public class AutoTest extends Auto{
     @Override
     public void runOpMode() throws InterruptedException {
 
+        reference(this);
+        activate();
+
+        waitForStart();
+
+        ready();
+
+        bot.tankDrive.move(0.4, 0);
+        sleep(450);
+        bot.tankDrive.move(0, 0.4);
+        sleep(920);
+        bot.tankDrive.move(0.4, 0);
+        sleep(1500);
+        bot.tankDrive.move(0,0.2);
+        bot.carousel.spin(1);
+        sleep(5000);
+        bot.carousel.spin(0);
+        bot.tankDrive.move(-0.4, 0);
+        sleep(480);
+        bot.tankDrive.move(0, -0.4);
+        sleep(850);
+        bot.tankDrive.move(0.4, 0);
+        sleep(1200);
+        bot.tankDrive.move(0, -0.4);
+        sleep(500);
+        bot.tankDrive.move(0,0);
+
+        end();
     }
 
 }
