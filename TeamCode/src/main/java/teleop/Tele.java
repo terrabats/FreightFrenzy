4ef6741ca96e0f1ddd5d5d69ead71065e0be3675
shuhul <shuhulmujoo@gmail.com>
2021-12-20 16:16:25 -0800

    v2.18.2 - Commented teleop packages - NOTE: Didn't comment teleop since it changes very often

--- TeamCode/src/main/java/teleop/Tele.java
+++ TeamCode/src/main/java/teleop/Tele.java
@@ -8,28 +8,39 @@ import global.General;
 import static global.General.*;
 
 public abstract class Tele extends OpMode implements Common {
-    // Base class for teleops
+    /**
+     * Base class for teleops
+     * NOTE: If the methods are overriden then make sure the call super.methodname
+     * this will generate by default if you use @Override
+     */
 
-
+    /**
+     * Init method runs when the user clicks the init button to run a teleop
+     */
     @Override
     public void init() {
         reference(this);
     }
-
+    /**
+     * Start method runs when the user clicks the start button after init
+     */
     @Override
     public void start() {
         ready();
     }
-
+    /**
+     * Loop method runs over and over after start
+     */
     @Override
     public void loop() {
         update(true);
     }
 
+    /**
+     * Stop method runs either when stop button is played or when the teleop ends
+     */
     @Override
     public void stop() {
         end();
     }
-
-
 }
