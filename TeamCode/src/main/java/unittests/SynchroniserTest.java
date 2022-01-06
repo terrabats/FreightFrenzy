6d367b78133d162394dc6d141c183e0f1b3e625a
varinik <varinikadakia@gmail.com>
2022-01-05 18:30:39 -0800

    v2.26.8 - Varini - Finished Synchroiser Test

---
+++ TeamCode/src/main/java/unittests/SynchroniserTest.java
@@ -0,0 +1,29 @@
+package unittests;
+import static global.General.*;
+
+import debugging.Synchroniser;
+import unittests.UnitTest;
+
+public class SynchroniserTest extends UnitTest {
+    Synchroniser synch = new Synchroniser();
+
+    @Override
+    protected void start() {
+        synch.resetDelay();
+    }
+
+    @Override
+    protected void loop() {
+        log.show("Did resetDelay actually reset everything? ",synch.getDelay());
+
+        if (gamepad1.a)
+        {
+            synch.update();
+        }
+    }
+
+    @Override
+    public void stop() {
+        synch.logDelay();
+    }
+}
