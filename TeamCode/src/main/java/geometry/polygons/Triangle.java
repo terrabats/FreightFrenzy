1164701b398524d3e3a943e68e5bf35070564b2c
shuhul <shuhulmujoo@gmail.com>
2022-01-15 18:17:03 -0800

    v3.2.4 - Moved Triangle methods into Trig

--- TeamCode/src/main/java/geometry/polygons/Triangle.java
+++ TeamCode/src/main/java/geometry/polygons/Triangle.java
@@ -18,18 +18,5 @@ public class Triangle extends Polygon {
         this.p3 = p3;
     }
     // TODO FIX
-    // Remove this from here put into trig
     // Make this class have other methods
-    //Calculates law of cosines for C knowing a,b and the angle between them
-    public static double lawOfCosinesC(double a, double b, double rad){
-        return Math.sqrt(a*a+b*b-(2*a*b*Math.cos(rad)));
-    }
-    //Calculates law of sines for the angle knowing a,b and the angle opposite to b
-    public static double lawOfSinesAngle(double a, double b, double bAng){
-        return Math.asin(a*Math.sin(bAng)*(1/b));
-    }
-    //Calculates the pythagorean theorem for the c knowing a and b
-    public static double pythagoreanC(double a, double b){
-        return Math.sqrt(a*a + b*b);
-    }
 }
--- TeamCode/src/main/java/math/trigonmetry/Trigonmetry.java
+++
@@ -1,6 +0,0 @@
-package math.trigonmetry;
-
-public class Trigonmetry {
-    // TODO NEW
-    // Add classes here for trig related stuffs
-}
