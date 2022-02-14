a5587942b1080a3ae0b9eac4138d70abec0af60f
Akhil Agarwal <akhilagarwal79@gmail.com>
2022-02-13 17:30:37 -0800

    v3.14.4 - Added Two Odometry to Terrabats code

--- TeamCode/src/main/java/robotparts/sensors/TwoOdometry.java
+++ TeamCode/src/main/java/robotparts/sensors/TwoOdometry.java
@@ -1,93 +1,90 @@
 package robotparts.sensors;
 
-import static java.lang.Math.PI;
-import static java.lang.Math.abs;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
+import geometry.position.Vector2;
+import robotparts.RobotPart;
+import robotparts.electronics.input.IEncoder;
+
 import static global.General.bot;
 import static robot.RobotFramework.odometryThread;
 
-import robotparts.RobotPart;
-import robotparts.electronics.input.IEncoder;
-import util.codeseg.ExceptionCodeSeg;
+public class TwoOdometry extends RobotPart {
 
-/**
- * NOTE: Uncommented
- */
+    private IEncoder horizontalEncoder;
+    private IEncoder verticalEncoder;
 
-public class TwoOdometry extends Odometry {
 
-    private final double ODO2_TO_CENTER_Y;
+    private double lastHorizontalEncoderPos;
+    private double lastVerticalEncoderPos;
 
-    private double prevOdoTwoPos = 0;
 
-    private final ExceptionCodeSeg<RuntimeException> odometryUpdateCode = this::update;
+    private final Vector2 positionOdometryCenter = new Vector2(0,0);
+    private Vector2 positionRobotCenter = new Vector2(0,0);
+    private final Vector2 localOdometryCenterOffset = new Vector2(3,-0.5);
+    private double heading = 0;
 
-    private IEncoder rEnc;
-
-    public TwoOdometry(double odo1_to_center_x, double odo2_to_center_y) {
-        super(odo1_to_center_x);
-        ODO2_TO_CENTER_Y = odo2_to_center_y;
-    }
 
     @Override
     public void init() {
-        super.init();
-        rEnc = createEncoder("br", "rEnc", IEncoder.Type.NORMAL);
-        update();
-        curPos = new double[] { 0, 0, 0 };
-        odometryThread.setExecutionCode(odometryUpdateCode);
-
+        horizontalEncoder = createEncoder("br", "cEnc", IEncoder.Type.NORMAL);
+        verticalEncoder = createEncoder("bl", "rEnc", IEncoder.Type.NORMAL);
+        lastHorizontalEncoderPos = horizontalEncoder.getPos();
+        lastVerticalEncoderPos = verticalEncoder.getPos();
+        odometryThread.setExecutionCode(this::update);
     }
 
-    private double getDeltaOdoTwo() {
-        double delta = -rEnc.getPos() - prevOdoTwoPos;
-        prevOdoTwoPos += delta;
+    public double getHorizontalEncoderPosition(){
+        return horizontalEncoder.getPos();
+    }
+
+    public double getVerticalEncoderPosition(){
+        return verticalEncoder.getPos();
+    }
+
+    private double getLocalHorizontalDelta(){
+        double delta = getHorizontalEncoderPosition() - lastHorizontalEncoderPos;
+        lastHorizontalEncoderPos = getHorizontalEncoderPosition();
         return ticksToCm(delta);
     }
 
-    private double ticksToCm(double ticks) {
+    private double getLocalVerticalDelta(){
+        double delta = getVerticalEncoderPosition() - lastVerticalEncoderPos;
+        lastVerticalEncoderPos = getVerticalEncoderPosition();
+        return ticksToCm(delta);
+    }
+
+    public double ticksToCm(double ticks) {
         return ticks/8192 * 3.5 * Math.PI;
     }
 
-    public double getCurX() { return curPos[0]; }
-    public double getCurY() { return curPos[1]; }
-    public double getCurThetaRad() {
-        return curPos[2];
+    private void updateHeading(){
+        heading = bot.gyroSensors.getRightHeadingRad();
     }
 
-    public void processTheta() {
-        curPos[2] %= (2 * Math.PI);
-        if (curPos[2] < 0) { curPos[2] += 2 * Math.PI; }
-        if (curPos[2] > Math.PI) { curPos[2] -= 2 * Math.PI; }
+    public void updatePosition(){
+        Vector2 localDelta = new Vector2(getLocalHorizontalDelta(), getLocalVerticalDelta());
+        Vector2 globalDelta = localDelta.getRotated(-heading);
+        positionOdometryCenter.add(globalDelta);
+        Vector2 globalOdometryCenterOffset = localOdometryCenterOffset.getRotated(heading);
+        positionRobotCenter = positionOdometryCenter.getAdded(globalOdometryCenterOffset).getAdded(localOdometryCenterOffset.getNegative());
     }
 
-    public double getCurThetaDeg() {
-        return getCurThetaRad() * 180/PI;
+    public void update(){
+        updateHeading();
+        updatePosition();
     }
 
-//    public void update() {
-//        double[] change = getPosChange();
-//        curPos[0] += change[0];
-//        curPos[1] += change[1];
-//        curPos[2] += change[2];
-//    }
-//
-//    // TODO: TEST THIS
-//    public double[] getPosChange() {
-//        lastChangePos = getPosChangeCenter();
-//        double[] posChange = new double[3];
-//        posChange[0] = lastChangePos[0] * cos(curPos[2]) + lastChangePos[1] * cos(curPos[2] + PI/2);
-//        posChange[1] = lastChangePos[0] * sin(curPos[2]) + lastChangePos[1] * sin(curPos[2] + PI/2);
-//        posChange[2] = lastChangePos[2];
-//        return posChange;
-//    }
-
-    // NOTE: Odometry modules are to the left and to the back of the center of the robot
-    public double[] getPosChangeCenter() {
-        double[] posChangeNoStrafe = super.getPosChangeCenter();
-        double dx = getDeltaOdoTwo() - posChangeNoStrafe[2] * ODO2_TO_CENTER_Y;
-        return new double[] { dx, posChangeNoStrafe[1], posChangeNoStrafe[2] };
-
+    public double getX(){
+        return positionRobotCenter.getX();
     }
+    public double getY(){
+        return positionRobotCenter.getY();
+    }
+    public double getHeading(){
+        return heading;
+    }
+
+    public double[] getPose(){
+        return new double[]{getX(), getY(), getHeading()};
+    }
+
 }
