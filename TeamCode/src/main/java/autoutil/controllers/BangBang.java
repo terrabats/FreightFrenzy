d3321fd725d7fb9cfc36634407faea71cc45f381
shuhul <shuhulmujoo@gmail.com>
2022-03-04 08:52:46 -0800

    v3.18.7 - Created BangBang controller

--- TeamCode/src/main/java/autoutil/controllers/BangBang.java
+++ TeamCode/src/main/java/autoutil/controllers/BangBang.java
@@ -1,6 +1,32 @@
 package autoutil.controllers;
 
-public class BangBang {
-    // TOD4 NEW
-    // Make this lol
+public class BangBang extends Controller{
+
+    private double s;
+    private double maximumTime;
+
+    public BangBang(double s, double maximumTime){
+        this.s = s;
+        this.maximumTime = maximumTime;
+    }
+
+    public void setMaximumTime(double maximumTime){ this.maximumTime = maximumTime; }
+
+    @Override
+    public void update() {
+        if(!isWithinAccuracyRange()) {
+            if (getError() > 0) {
+                output = s;
+            } else {
+                output = -s;
+            }
+        }else{
+            if((errorProfiler.getCurrentTime() - currentTime) > maximumTime){
+                isAtTarget = true;
+            }else{
+                output = 0;
+                currentTime = errorProfiler.getCurrentTime();
+            }
+        }
+    }
 }
