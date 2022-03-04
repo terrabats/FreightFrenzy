9bdb9e87d2443ba61510b4f7965ce4116c39160f
Akhil Agarwal <akhilagarwal79@gmail.com>
2022-03-04 15:59:45 -0800

    v3.18.12 - Commented Arc Generator and some PathOld

--- TeamCode/src/main/java/autoutil/generators/ArcGenerator.java
+++ TeamCode/src/main/java/autoutil/generators/ArcGenerator.java
@@ -12,12 +12,13 @@ import geometry.position.Pose;
 
 import static java.lang.Math.*;
 
+/**
+ * Generates the main arcing motion of the robot
+ */
 public class ArcGenerator {
-    // Generates a path for the robot to follow
-    // based on a array of points or conditions
-    // there should be one static generator
-    // should be able to create multiple paths if a decision is needed
-
+    /**
+     * Stores all of the positions from the arc
+     */
     private final ArrayList<Pose> poses = new ArrayList<>();
 
 //    public void addNewPos(double x, double y, double ang) {
@@ -30,12 +31,26 @@ public class ArcGenerator {
 //        moveTo(p);
 //    }
 
+    /**
+     * Adds a new point (with more easy-to-use parameters)
+     * @param x Absolute x on the field
+     * @param y Absolute y on the field
+     * @param ang Absolute heading on the field
+     */
     public void moveTo(double x, double y, double ang) {
         moveTo(new Pose(new Point(x, y), ang));
     }
 
+    /**
+     * Adds a new point
+     * @param p Absolute position to move to
+     */
     private void moveTo(Pose p) { poses.add(p); }
 
+    /**
+     * Marks the path as complete
+     * @return completed path
+     */
     public PathOld done() {
         PathOld path = new PathOld();
         for (int i = 0; i < poses.size() - 1; i++) {
@@ -44,6 +59,12 @@ public class ArcGenerator {
         return path;
     }
 
+    /**
+     * Generates the segments between two points
+     * @param p1 first point
+     * @param p2 second point
+     * @return Segments to go between two points with arcs
+     */
     private ArrayList<PathSegment> generateSeg(Pose p1, Pose p2) {
         ArrayList<PathSegment> ret = generateRelSeg(p1, p2);
         p1.rotate(-PI/2);
@@ -71,13 +92,22 @@ public class ArcGenerator {
         return ret;
     }
 
+    /**
+     * Generates relative segments between two different points
+     * @param p1 First position
+     * @param p2_orig Second position
+     * @return Relative segments between points
+     */
     private ArrayList<PathSegment> generateRelSeg(Pose p1, Pose p2_orig) {
+        // Final list to return
         ArrayList<PathSegment> ret = new ArrayList<>();
 
+        // Making it relative (for calculation)
         p1.rotate(-PI/2);
         Pose p2 = p2_orig.getRelativeTo(p1);
         p1.rotate(PI/2);
 
+        // Normalizing angle
         p2.ang %= 2 * PI;
         if (p2.ang > PI) { p2.ang -= 2 * PI; }
 
@@ -88,12 +118,14 @@ public class ArcGenerator {
         boolean isNegPiBy2 = p2.ang == -PI/2;
         if (isNegPiBy2) p2.ang *= -1;
 
+        // Flipping (for generation)
         boolean flipX = p2.p.x < 0;
         boolean flipY = p2.p.y < 0;
 
         p2.p.x = Math.abs(p2.p.x);
         p2.p.y = Math.abs(p2.p.y);
 
+        // Core math to generate arc
         double p = p2.ang - PI/2;
         double dx = p2.p.x;
         double dy = p2.p.y;
@@ -111,6 +143,7 @@ public class ArcGenerator {
         Circle c1;
         Circle c2;
 
+        // choose between the two circles
         if (abs(r) < abs(r2)) {
             c1 = new Circle(new Point(r, 0), abs(r));
             c2 = new Circle(new Point(dx + r * cos(p), dy + r * sin(p)), abs(r));
@@ -119,22 +152,28 @@ public class ArcGenerator {
             c2 = new Circle(new Point(dx + r2 * cos(p), dy + r2 * sin(p)), abs(r2));
         }
 
+        // Find point of intersection efficiently
         PathLine betweenCenters = new PathLine(c1.center, c2.center);
         Point pointOfIntersection = betweenCenters.getAt(0.5);
 
+        // Get the shorter arc from each point to the intersection
         PathArc cir1arc = getShorterArc(c1, new Point(0, 0), pointOfIntersection);
         PathArc cir2arc = getShorterArc(c2, p2.p, pointOfIntersection);
 
         if (isNegPiBy2) p2.ang *= -1;
 
         if (cir1arc.goingCW(new Pose(new Point(0, 0), PI/2)) == cir2arc.goingCW(p2)) {
-            // TAKE SHORTCUT
+            // TAKE TANGENT LINE BETWEEN
             double mx = c2.center.x - c1.center.x;
             double my = c2.center.y - c1.center.y;
 
             if (mx != 0) {
+                // Tangent line is non-vertical
+
+                // Calculate slope of the tangent line
                 double mt = my / mx;
 
+                // Calculate the tangent lines
                 double temp1 = (c1.r * mt) / sqrt(pow(mt, 2) + 1);
 
                 double x1 = -temp1 + c1.center.x;
@@ -157,10 +196,12 @@ public class ArcGenerator {
 
                 PathLine tangentLine2 = new PathLine(new Point(x1, y1), new Point(x2, y2));
 
+                // Finding arcs to go with the tangent lines
                 PathArc a21 = getShorterArc(c1, new Point(0, 0), new Point(x1, y1));
                 PathArc a22 = getShorterArc(c2, new Point(x2, y2), p2.p);
                 double arcLensPath2 = a21.getArcLength() + a22.getArcLength();
 
+                // Find which path would be longer and choosing the shorter
                 if (arcLensPath1 < arcLensPath2) {
                     ret.add(a11);
                     ret.add(tangentLine1);
@@ -171,26 +212,41 @@ public class ArcGenerator {
                     ret.add(a22);
                 }
             } else {
+                // Tangent line is vertical
                 ret.add(new PathLine(new Point(0, 0), new Point(0, c2.center.y)));
                 ret.add(getShorterArc(c2, new Point(0, c2.center.y), p2.p));
             }
         } else {
+            // Simple: add the two arcs
             ret.add(cir1arc);
             ret.add(cir2arc);
         }
 
+        // Flip back
         for (PathSegment pathSegment : ret) {
             pathSegment.flip(flipX, flipY);
         }
 
+        // Return value
         return ret;
     }
 
+    /**
+     * Find and return the arc on a circle with less length (shortest-path finder)
+     * @param c Circle
+     * @param p1 First point on circle
+     * @param p2 Second point on circle
+     * @return Shorter arc along circle c with p1 and p2
+     */
     private PathArc getShorterArc(Circle c, Point p1, Point p2) {
         PathArc arc1 = new PathArc(c, p1, p2, true);
         PathArc arc2 = new PathArc(c, p2, p1, false);
         return arc1.getArcLength() > arc2.getArcLength() ? arc2 : arc1;
     }
 
+    /**
+     * Will the generator generate anything?
+     * @return Generator empty?
+     */
     public boolean empty() { return poses.size() < 2; }
 }
--- TeamCode/src/main/java/autoutil/paths/PathOld.java
+++ TeamCode/src/main/java/autoutil/paths/PathOld.java
@@ -2,11 +2,17 @@ package autoutil.paths;
 
 import java.util.ArrayList;
 
+/**
+ * Stores the full path of the robot
+ */
 public class PathOld {
     // Composed of multiple path segments
     // Has the information about where the robot should be at a given time or distance
     // Includes any other relevant information to execute the path
 
+    /**
+     * Stores all of the path segments
+     */
     public final ArrayList<PathSegment> segments = new ArrayList<>();
 
     public void addSegments(ArrayList<PathSegment> p) { segments.addAll(p); }
