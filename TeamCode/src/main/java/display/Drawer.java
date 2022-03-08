b3ac471ef16a6c01b82f724f3b29c0a7fc2ed813
shuhul <shuhulmujoo@gmail.com>
2022-03-08 13:57:20 -0800

    v3.21.7 - Created Drawing of field, untested

--- TeamCode/src/main/java/display/Drawer.java
+++ TeamCode/src/main/java/display/Drawer.java
@@ -4,7 +4,11 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -15,6 +19,7 @@ import geometry.position.Line;
 import geometry.position.Point;
 import geometry.position.Pose;
 import geometry.position.Vector;
+import util.ExceptionCatcher;
 
 public class Drawer extends JPanel {
     protected Graphics2D g;
@@ -63,43 +68,74 @@ public class Drawer extends JPanel {
         g.drawOval((int) c.x, (int) c.y, (int) r,(int) r);
     }
 
-    public void whatToDraw(){
-        Triangle triangle = new Triangle(new Point(200,240), new Point(300,180), new Point(260,160));
-        double area = triangle.area();
-        Rect bbox = triangle.boundingbox();
-        System.out.println(area);
-        System.out.println(bbox.toString());
-        Point p1 = triangle.points.get(0);
-        Point p2 = triangle.points.get(1);
-        Point p3 = triangle.points.get(2);
-        drawLine(new Line(p1, p2));
-        drawLine(new Line(p2, p3));
-        drawLine(new Line(p1, p3));
-        drawPoint(p1);
-        drawPoint(p2);
-        drawPoint(p3);
-        Point r1 = new Point(bbox.getX1(), bbox.getY1());
-        Point r2 = new Point(bbox.getX1(), bbox.getY2());
-        Point r3 = new Point(bbox.getX2(), bbox.getY2());
-        Point r4 = new Point(bbox.getX2(), bbox.getY1());
-        pointColor = Color.RED;
-        lineColor = Color.BLUE;
-        drawLine(new Line(r1, r2));
-        drawLine(new Line(r2, r3));
-        drawLine(new Line(r3, r4));
-        drawLine(new Line(r4, r1));
-        drawPoint(r1);
-        drawPoint(r2);
-        drawPoint(r3);
-        drawPoint(r4);
+    public void drawField(){
+        final BufferedImage image;
+//        System.out.println();
+//        File f = new File("display/ff.png");
+//        System.out.println(System.getProperty("user.dir")+ "\\TeamCode\\src\\main\\java\\display");
+        File f = new File(System.getProperty("user.dir")+ "\\TeamCode\\src\\main\\java\\display\\ff2.png");
+//        System.out.println("Heei");
+//        if(f.exists()) {
+//            System.out.println("Hiii");
+//        }
+        try {
+//            File f2 = new File("ff.png");
+//            if(f.exists() && !f.isDirectory()) {
+//
+//            }
+            image = ImageIO.read(f);
+            g.drawImage(image, 0, 0, 780, 770, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
+//    public void whatToDraw(){
+//        Triangle triangle = new Triangle(new Point(200,240), new Point(300,180), new Point(260,160));
+//        double area = triangle.area();
+//        Rect bbox = triangle.boundingbox();
+//        System.out.println(area);
+//        System.out.println(bbox.toString());
+//        Point p1 = triangle.points.get(0);
+//        Point p2 = triangle.points.get(1);
+//        Point p3 = triangle.points.get(2);
+//        drawLine(new Line(p1, p2));
+//        drawLine(new Line(p2, p3));
+//        drawLine(new Line(p1, p3));
+//        drawPoint(p1);
+//        drawPoint(p2);
+//        drawPoint(p3);
+//        Point r1 = new Point(bbox.getX1(), bbox.getY1());
+//        Point r2 = new Point(bbox.getX1(), bbox.getY2());
+//        Point r3 = new Point(bbox.getX2(), bbox.getY2());
+//        Point r4 = new Point(bbox.getX2(), bbox.getY1());
+//        pointColor = Color.RED;
+//        lineColor = Color.BLUE;
+//        drawLine(new Line(r1, r2));
+//        drawLine(new Line(r2, r3));
+//        drawLine(new Line(r3, r4));
+//        drawLine(new Line(r4, r1));
+//        drawPoint(r1);
+//        drawPoint(r2);
+//        drawPoint(r3);
+//        drawPoint(r4);
+//    }
+
+
+    public void define(){
+        drawField();
+//        System.out.println("Hi");
+    }
 
     @Override
     public void paintComponent(Graphics g) {
         this.g = (Graphics2D) g;
-        whatToDraw();
+//        System.out.println("Hi");
+        define();
+//        whatToDraw();
     }
+
 
 
     public static void drawWindow(int width, int height, String name){
